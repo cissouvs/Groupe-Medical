a8686197149871e57a613793a89fdff0daca196a
Leskeu <pro.theotherage@gmail.com>
2026-07-21 13:11:22 +0200

    init commit

---
+++ Memora/MemoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoraApp.swift
+//  Memora
+//
+//  Created by Leskeu  on 21/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct MemoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

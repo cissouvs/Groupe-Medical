9b78ad52ca069789d2d88c180ffdfcd7e72e5425
Gillian <pasquettegillian@sfr.fr>
2026-07-23 16:43:48 +0200

    init Event creation sheet

--- Memora/UI/CalendarView.swift
+++ Memora/UI/CalendarView.swift
@@ -9,6 +9,13 @@ import SwiftUI
 
 struct CalendarView: View {
     @State var date = Date()
+    @State var isAddSheetPresented: Bool = true
+    @State var eventTitle: String = ""
+    @State var eventDescription: String = ""
+    @State var eventType: EventType = .other
+    @State var isEventAllDay: Bool = false
+    @State var eventDate: Date = Date()
+
     var calendar = Calendar.current
 
     var events: [Event]
@@ -20,7 +27,7 @@ struct CalendarView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
-                DatePicker("test", selection: $date, displayedComponents: [.date])
+                DatePicker("", selection: $date, displayedComponents: [.date])
                     .datePickerStyle(.graphical)
                     .environment(\.locale, Locale.init(identifier: "fr"))
                     .tint(.accent)
@@ -36,10 +43,32 @@ struct CalendarView: View {
                 }
                 ToolbarItem(placement: .topBarTrailing) {
                     Button {
-
+                        isAddSheetPresented = true
                     } label: {
                         Image(systemName: "plus")
                             .foregroundStyle(.black)
+                    }
+                }
+            }
+            .sheet(isPresented: $isAddSheetPresented) {
+                Form {
+                    Section {
+                        TextField("Titre", text: $eventTitle)
+                        TextField("Description", text: $eventDescription)
+                        Picker("Type", selection: $eventType) {
+                            ForEach(EventType.allCases) { type in
+                                Text(type.rawValue).tag(type)
+                            }
+                        }
+                        .tint(.accent)
+                    }
+
+                    Section {
+                        Toggle("Journée Entière", isOn: $isEventAllDay)
+                        DatePicker("Début", selection: $eventDate)
+                            .datePickerStyle(.automatic)
+                        DatePicker("Fin", selection: $eventDate)
+                            .datePickerStyle(.automatic)
                     }
                 }
             }

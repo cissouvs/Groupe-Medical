5fd646f027830b925d60d171f8ddfa3c0c80405a
Gillian <pasquettegillian@sfr.fr>
2026-07-23 15:31:45 +0200

    Add .DS_Store to .gitignore

---
+++ Memora/UI/EventListElementView.swift
@@ -0,0 +1,18 @@
+//
+//  EventListElementView.swift
+//  Memora
+//
+//  Created by Apprenant76 on 23/07/2026.
+//
+
+import SwiftUI
+
+struct EventListElementView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventListElementView()
+}

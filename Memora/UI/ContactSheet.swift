65ad122a134ad535b9ddd1d4e9ec1cc96a50e3bb
Leskeu <pro.theotherage@gmail.com>
2026-07-28 16:42:07 +0200

    end of the day commit

--- Memora/UI/ContactSheet.swift
+++ Memora/UI/ContactSheet.swift
@@ -6,27 +6,51 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContactSheet: View {
-    @State var contactName : String = ""
-    @State var contactPrenom : String = ""
+    @Environment(\.dismiss) var dismiss
+    @State var pickerItem : PhotosPickerItem?
+    @State var pickerImage : Image?
+    @State var contactForm : Contact = Contact(
+        emailAdress: "",
+        firstName: "",
+        surName: "",
+        phoneNumber: "",
+        adress: "",
+        postalCode: "",
+        photo: Image("")
+    )
+    @Binding var contacts : [Contact]
     var body: some View {
         VStack {
             HStack(spacing: 30){
                 Button {
-                    
+                    dismiss()
                 } label: {
                     Image(systemName: "xmark")
                         .foregroundStyle(.mainText)
                         .padding()
                         .background(.ultraThinMaterial)
                         .clipShape(.circle)
-                        
+                    
                 }
                 Text("Ajoutez un contact")
                     .font(.title)
                 Button {
-                    
+                    contacts
+                        .append(
+                            Contact.init(
+                                emailAdress: contactForm.emailAdress,
+                                firstName: contactForm.firstName,
+                                surName: contactForm.surName,
+                                phoneNumber: contactForm.phoneNumber,
+                                adress: contactForm.adress,
+                                postalCode: contactForm.postalCode,
+                                photo: contactForm.photo
+                            )
+                        )
+                    dismiss()
                 } label: {
                     Image(systemName: "checkmark")
                         .foregroundStyle(.white)
@@ -35,13 +59,42 @@ struct ContactSheet: View {
                         .clipShape(.circle)
                 }
             }
+            PhotosPicker(selection: $pickerItem, matching: .images) {
+                if pickerImage == nil {
+                    Image("placeholder")
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(.circle)
+                        .frame(maxWidth: 200)
+                } else {
+                    Image("colette")
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(.circle)
+                        .frame(maxWidth: 200)
+                }
+            }
+            .onChange(of: pickerItem) {
+                Task {
+                    guard let pickerItem else {
+                        return
+                    }
+                    if let newImage = try? await pickerItem.loadTransferable(type: Image.self) {
+                        pickerImage = newImage
+                    }
+                }
+            }
             Form {
-                TextField("Nom", text: $contactName)
-                TextField("Prenom")
+                TextField("Nom", text: $contactForm.surName)
+                TextField("Prenom", text: $contactForm.firstName)
+                TextField("Adresse Mail", text: $contactForm.emailAdress)
+                TextField("Numéro de téléphone", text: $contactForm.phoneNumber)
+                TextField("Adresse", text: $contactForm.adress)
             }
         }
     }
 }
+
 #Preview {
-    ContactSheet()
+    ContactSheet(contacts: .constant(emergencyContacts))
 }
--- Memora/UI/ContactsListView.swift
+++ Memora/UI/ContactsListView.swift
@@ -8,47 +8,54 @@
 import SwiftUI
 
 struct ContactsListView: View {
-    let contacts : [Contact]
+    @State var contacts : [Contact]
+    @State var selectedContact : Contact?
+    @State var isSelected : Bool = false
     var body: some View {
         ZStack {
             Color.background
             NavigationStack {
                 VStack {
                     List {
-                            ForEach(contacts) { contact in
-                                NavigationLink {
-                                    ContactDetailView(contact: contact)
-                                } label: {
-                                    HStack {
-                                        Image(contact.photo)
-                                            .resizable()
-                                            .scaledToFit()
-                                            .clipShape(.circle)
-                                            .frame(maxWidth: 50)
-                                        Text(contact.firstName)
-                                        Text(contact.surName)
-                                    }
-                                }
-                            }
-                    }.font(.title3)
-                        .toolbar {
-                            ToolbarItem(placement: .principal){
-                                Text("Contacts d'urgence")
-                                    .font(.title)
-                                    .bold()
-                            }
-                            ToolbarItem(placement: .topBarTrailing) {
-                                Button {
-                                    
-                                } label: {
-                                    Image(systemName: "plus")
+                        ForEach(contacts) { contact in
+                            NavigationLink {
+                                ContactDetailView(contact: contact)
+                            } label: {
+                                HStack {
+                                    contact.photo
+                                        .resizable()
+                                        .scaledToFit()
+                                        .clipShape(.circle)
+                                        .frame(maxWidth: 50)
+                                    Text(contact.firstName)
+                                    Text(contact.surName)
                                 }
                             }
                         }
+                    }
+                    .sheet(isPresented: $isSelected){
+                        ContactSheet(contacts: $contacts)
+                            .padding(.vertical)
+                }
+                .font(.title3)
+                .toolbar {
+                    ToolbarItem(placement: .principal){
+                        Text("Contacts d'urgence")
+                            .font(.title)
+                            .bold()
+                    }
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button {
+                            isSelected = true
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                    }
                 }
             }
         }
     }
+}
 }
 #Preview {
     ContactsListView(contacts: emergencyContacts)

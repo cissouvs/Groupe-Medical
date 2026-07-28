4e35028bdca12937a27c8b947407286c8842a0c9
cissouvs <illiencyriac13@gmail.com>
2026-07-28 09:41:37 +0200

    Create an MapView

---
+++ Memora/UI/MapView.swift
@@ -0,0 +1,22 @@
+//
+//  MapView.swift
+//  Memora
+//
+//  Created by apprenant92 on 23/07/2026.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    
+    
+    var body: some View {
+        
+        Map()
+    }
+}
+
+#Preview {
+    MapView()
+}

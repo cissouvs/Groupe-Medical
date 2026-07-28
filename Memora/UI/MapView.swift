e68812a29189cc82ff89b2a69478a6c052dabe51
cissouvs <illiencyriac13@gmail.com>
2026-07-28 11:02:24 +0200

    Create careGiversPosition and add button for refocus on the careGiversPosition

--- Memora/UI/MapView.swift
+++ Memora/UI/MapView.swift
@@ -12,11 +12,17 @@ struct MapView: View {
     
     @State var camera = MapCameraPosition.automatic
     
-    var userPosition = CLLocationCoordinate2D(latitude: 44.340861, longitude: 1.2024031597201768)
+    var userPosition = CLLocationCoordinate2D(
+        latitude: 44.340861,
+        longitude: 1.2024031597201768)
+    
+    var careGiversPosition = CLLocationCoordinate2D(
+        latitude: 44.33878193104696,
+        longitude: 1.2116824676429137)
     
     var body: some View {
         
-        ZStack{
+        ZStack(alignment: .bottomTrailing) {
             
             Map(position: $camera) {
                 
@@ -28,27 +34,49 @@ struct MapView: View {
                         .clipShape(.circle)
                 }
                 
+                Annotation("Patient", coordinate: careGiversPosition) {
+                    Image(systemName: "person")
+                        .foregroundStyle(.white)
+                        .padding()
+                        .background(.supportRed)
+                        .clipShape(.circle)
+                }
+                
             }
             
             VStack(alignment: .trailing){
                 
                 Spacer()
+                
+                Button {
+                    camera = .region(MKCoordinateRegion(
+                        center: careGiversPosition,
+                        latitudinalMeters: 200,
+                        longitudinalMeters: 200))
+                } label: {
+                    Image(systemName: "mappin")
+                        .font(.title)
+                        .foregroundStyle(.white)
+                        .padding(10)
+                        .background(.supportRed)
+                        .clipShape(.circle)
+                }
+                
                 Button {
                     camera = .region(MKCoordinateRegion(
                         center: userPosition,
                         latitudinalMeters: 200,
                         longitudinalMeters: 200))
                 } label: {
-                    Image(systemName: "person")
+                    Image(systemName: "mappin")
+                        .font(.title)
                         .foregroundStyle(.white)
-                        .padding()
+                        .padding(10)
                         .background(.supportBlue)
                         .clipShape(.circle)
                 }
-                
-                
             }
-            
+            .padding()
         }
     }
 }

5998d7d174f43a80c0722747b9a229e8680cd636
cissouvs <illiencyriac13@gmail.com>
2026-07-28 10:48:32 +0200

    Create userPosition and add button for refocus on the userPosition

--- Memora/UI/MapView.swift
+++ Memora/UI/MapView.swift
@@ -10,10 +10,46 @@ import MapKit
 
 struct MapView: View {
     
+    @State var camera = MapCameraPosition.automatic
+    
+    var userPosition = CLLocationCoordinate2D(latitude: 44.340861, longitude: 1.2024031597201768)
     
     var body: some View {
         
-        Map()
+        ZStack{
+            
+            Map(position: $camera) {
+                
+                Annotation("Patient", coordinate: userPosition) {
+                    Image(systemName: "person")
+                        .foregroundStyle(.white)
+                        .padding()
+                        .background(.supportBlue)
+                        .clipShape(.circle)
+                }
+                
+            }
+            
+            VStack(alignment: .trailing){
+                
+                Spacer()
+                Button {
+                    camera = .region(MKCoordinateRegion(
+                        center: userPosition,
+                        latitudinalMeters: 200,
+                        longitudinalMeters: 200))
+                } label: {
+                    Image(systemName: "person")
+                        .foregroundStyle(.white)
+                        .padding()
+                        .background(.supportBlue)
+                        .clipShape(.circle)
+                }
+                
+                
+            }
+            
+        }
     }
 }
 

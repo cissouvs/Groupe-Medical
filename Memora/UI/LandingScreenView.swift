f31e85b4aeab30a55fc960a8cb1e496a3472ea41
Gillian <pasquettegillian@sfr.fr>
2026-07-29 16:44:25 +0200

    Init LandingScreen

---
+++ Memora/UI/LandingScreenView.swift
@@ -0,0 +1,27 @@
+//
+//  LandingScreenView.swift
+//  Memora
+//
+//  Created by Apprenant76 on 29/07/2026.
+//
+
+import SwiftUI
+
+struct LandingScreenView: View {
+    var body: some View {
+        NavigationStack {
+            VStack(alignment: .leading) {
+                HStack {
+                    VStack {
+                        Text("Bonjour,")
+                        Text("Mr Yann")
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    LandingScreenView()
+}

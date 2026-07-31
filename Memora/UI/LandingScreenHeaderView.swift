1f91d8e68a0d32afe82dc1208005083ffffd7686
Gillian <pasquettegillian@sfr.fr>
2026-07-31 16:43:46 +0200

    set navigation for ProfileView

--- Memora/UI/LandingScreenHeaderView.swift
+++ Memora/UI/LandingScreenHeaderView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct LandingScreenHeaderView: View {
 
+    @Binding var  path: [Screen]
 
     var body: some View {
         HStack {
@@ -28,10 +29,10 @@ struct LandingScreenHeaderView: View {
                         .font(.title)
                 }
                 Button {
-
+                    path.append(.profile)
                 } label: {
-                    Image(systemName: "gearshape")
-                        .tint(.secondText)
+                    Image(systemName: "person")
+                        .tint(.accent)
                         .font(.title)
                 }
             }
@@ -40,5 +41,5 @@ struct LandingScreenHeaderView: View {
 }
 
 #Preview {
-    LandingScreenHeaderView()
+    LandingScreenHeaderView(path: .constant([]))
 }

fac95664a56c8d22c3f1379c27677187a20bbd32
Gillian <pasquettegillian@sfr.fr>
2026-07-31 14:26:39 +0200

    add AppointmentCardView

---
+++ Memora/UI/AppointmentCardView.swift
@@ -0,0 +1,58 @@
+//
+//  AppointmentCardView.swift
+//  Memora
+//
+//  Created by Apprenant76 on 30/07/2026.
+//
+
+import SwiftUI
+
+struct AppointmentCardView: View {
+
+    var body: some View {
+        HStack(spacing: 10) {
+            Image(systemName: "person.fill")
+                .resizable()
+                .padding(15)
+                .background(Color.gray)
+                .clipShape(.circle)
+                .frame(width: 70, height: 70)
+                .clipped()
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Dr Martin")
+                Text("Médecin Généraliste")
+                    .foregroundStyle(Color.secondText)
+                    .font(.subheadline)
+            }
+            .padding(10)
+            .frame(maxWidth: .infinity, alignment: .topLeading)
+            VStack(alignment: .center, spacing: 10) {
+                Text("11:00")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                HStack(alignment: .center, spacing: 15) {
+                    Button {
+
+                    } label: {
+                        Image(systemName: "phone.circle.fill")
+                            .font(.largeTitle)
+                    }
+                    Button {
+
+                    } label: {
+                        Image(systemName: "message.circle.fill")
+                            .font(.largeTitle)
+                    }
+                }
+            }
+            .padding(.horizontal, 5)
+        }
+        .padding(10)
+        .background(.whiteBackground)
+        .cornerRadius(20)
+    }
+}
+
+#Preview {
+    AppointmentCardView()
+}

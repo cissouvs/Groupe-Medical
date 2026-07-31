68521dd05a3ee1f4a14b9df161c7dc7c6a3ce2a5
Gillian <pasquettegillian@sfr.fr>
2026-07-31 14:27:49 +0200

    set LandingScreen UI

--- Memora/UI/LandingScreenView.swift
+++ Memora/UI/LandingScreenView.swift
@@ -6,18 +6,46 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct LandingScreenView: View {
     var body: some View {
         NavigationStack {
-            VStack(alignment: .leading) {
-                HStack {
-                    VStack {
-                        Text("Bonjour,")
-                        Text("Mr Yann")
+            ZStack {
+                Color.background
+                VStack(alignment: .leading, spacing: 10) {
+                    ScrollView {
+                        LandingScreenHeaderView()
+                        Divider()
+                        VStack(alignment: .leading, spacing: 20) {
+                            Text("Prochain Rendez-Vous")
+                                .foregroundStyle(.secondText)
+                            AppointmentCardView()
+                            Text("Rappel")
+                                .foregroundStyle(.secondText)
+                            MedicineCardView()
+                            Text("Vue d'ensemble")
+                            HStack {
+                                LandingScreenCalendarButtonView(calendarViewType: .medecine)
+                                Spacer()
+                                LandingScreenCalendarButtonView(calendarViewType:.appointment)
+                                Spacer()
+                                LandingScreenCalendarButtonView(calendarViewType: .event)
+                            }
+                            .padding(10)
+                            .background(.whiteBackground)
+                            .cornerRadius(20)
+                            Text("S'exercer")
+                                .foregroundStyle(.secondText)
+                            LandingScreenQuizzButtonView()
+
+                        }
                     }
                 }
+                .padding(.horizontal, 12)
+                .padding(.top, 60)
             }
+            .ignoresSafeArea()
         }
     }
 }

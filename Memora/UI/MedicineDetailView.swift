0b89f3b0cc1bafbd2e5fb2e2eda6d0cf587c5389
Gillian <pasquettegillian@sfr.fr>
2026-08-07 17:19:53 +0200

    update MedicineDetailView

--- Memora/UI/MedicineDetailView.swift
+++ Memora/UI/MedicineDetailView.swift
@@ -10,53 +10,94 @@ import SwiftUI
 var dateFormatter: DateFormatter {
     let formatter = DateFormatter()
     formatter.locale = Locale(identifier: "fr_FR")
-    formatter.setLocalizedDateFormatFromTemplate("d MMMM")
+    formatter.setLocalizedDateFormatFromTemplate("dd/MM")
     return formatter
 }
 
 struct MedicineDetailView: View {
 
     @State private var vm = MedicineDetailViewModel()
+    @State private var isDeleteConfirmationPresented = false
+    @Binding var path: [Screen]
     @Environment(MedecineViewModel.self) var medicineVM
+    @Environment(\.dismiss) private var dismiss
 
     let medicineId: UUID
 
-    var medicine: any Medicine {
+    var medicine: (any Medicine)? {
         medicineVM.getMedicine(medicineID: medicineId)
     }
 
     var body: some View {
-        VStack(spacing: 50) {
-            AsyncImage(url: URL(string: medicine.imageUrl)) { image in
-                image
-                    .resizable()
-            } placeholder: {
-                Image(systemName: "pills.fill")
-                    .resizable()
-                    .foregroundStyle(.accent)
+        VStack {
+            if let medicine {
+                VStack(spacing: 50) {
+                    AsyncImage(url: URL(string: medicine.imageUrl)) { image in
+                        image
+                            .resizable()
+                    } placeholder: {
+                        Image(systemName: "pills.fill")
+                            .resizable()
+                            .foregroundStyle(.accent)
+                    }
+                    .shadow(radius: 5)
+                    .frame(width: 250, height: 250)
+                    .clipped()
+                    .padding(.top, 100)
+                    MedicineDetailContenView(medicine: medicine)
+                }
+                .alert("Voulez-vous vraiment supprimer ce médicament?", isPresented: $isDeleteConfirmationPresented) {
+                    Button("Annuler", role: .cancel) {}
+                    Button("Supprimer", role: .destructive) {
+                        medicineVM.deleteMedicine(medicineID: medicine.id)
+                        path.removeLast()
+                    }
+                }
+                .toolbar {
+                    ToolbarItem(placement: .destructiveAction) {
+                        Button {
+                            isDeleteConfirmationPresented = true
+                        } label: {
+                            Image(systemName: "trash")
+                        }
+                    }
+                }
             }
-            .shadow(radius: 5)
-            .frame(width: 250, height: 250)
-            .clipped()
-            .padding(.top, 100)
-            VStack(alignment: .leading) {
+            else {
+                ContentUnavailableView {
+                    Label("Ce médicament est introuvable", systemImage: "pills")
+                }
+            }
+        }
+        .navigationTitle("Infos Médicament")
+        .ignoresSafeArea()
+    }
+}
+
+struct MedicineDetailContenView: View {
+    var medicine: any Medicine
+
+    var body: some View {
+        VStack(alignment: .center, spacing: 20) {
+            VStack(alignment: .leading, spacing: 20) {
                 Text(medicine.medicineType.rawValue)
                     .font(.title)
                     .bold()
                     .frame(maxWidth: .infinity, alignment: .leading)
-                Text(medicine.details)
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                ScrollView {
+                    Text(medicine.details)
+                        .foregroundStyle(.secondText)
+                }
+                .frame(height: 100)
+                TakingTimingScrollView(takingMoments: medicine.takingMoments)
                 InfoCardSectionView(medicine: medicine)
-                Spacer()
             }
-            .padding(12)
-            .frame(maxHeight: .infinity)
-            .background(Color.secondText)
-            .foregroundStyle(.white)
-            .cornerRadius(30)
+            Spacer()
         }
-        .ignoresSafeArea()
-        .navigationTitle("Infos Médicament")
+        .padding(12)
+        .background(Color.background)
+        .cornerRadius(30)
+        .shadow(radius: 3)
     }
 }
 
@@ -65,18 +106,40 @@ struct InfoCardSectionView: View {
     let medicine: any Medicine
 
     var body: some View {
-        HStack {
-            InfoCardView(medicine: medicine, cardType: .date)
+        VStack(alignment: .center) {
+            Grid {
+                switch medicine {
+                case is DrinkableMedicineModel:
+                    GridRow {
+                        InfoCardView(medicine: medicine, cardType: .date)
+                        InfoCardView(medicine: medicine, cardType: .volume)
+                    }
+                case is CapsuleMedicineModel:
+                    GridRow {
+                        InfoCardView(medicine: medicine, cardType: .date)
+                        InfoCardView(medicine: medicine, cardType: .amount)
+                    }
+                    GridRow {
+                        InfoCardView(medicine: medicine, cardType: .capsuleWeight)
+                    }
+                case is PatchMedicineModel:
+                    GridRow {
+                        InfoCardView(medicine: medicine, cardType: .date)
+                        InfoCardView(medicine: medicine, cardType: .patchDuration)
+                    }
+                default: VStack{}
+                }
+            }
         }
     }
 }
 
-enum InfoCardType {
-    case date
-    case amount
-    case capsuleWeight
-    case volume
-    case patchDuration
+enum InfoCardType: String {
+    case date = "Date de prise"
+    case amount = "Quantité"
+    case capsuleWeight = "Grammage"
+    case volume = "Volume"
+    case patchDuration = "Durée du patch"
 }
 
 struct InfoCardView: View {
@@ -99,28 +162,58 @@ struct InfoCardView: View {
         }
     }
 
-    var body: some View {
-        VStack(spacing: 10) {
-            HStack {
-                Image(systemName: "calendar")
-                    .font(.title)
-                Text("Date de prise")
-                    .bold()
-            }
-            Text(
-                "Du \(dateFormatter.string(from: medicine.startDate).capitalized) au \(dateFormatter.string(from: medicine.endDate).capitalized)"
-            )
-            .font(.title3)
+    var color: Color {
+        switch cardType {
+        case .date:
+                .accent
+        case .amount, .volume, .patchDuration:
+                .supportGreen
+        case .capsuleWeight:
+                .supportOrange
         }
-        .padding(10)
-        .background(Color.background)
-        .foregroundStyle(.accent)
+    }
+
+    var text: String {
+        switch cardType {
+        case .date:
+            return "\(dateFormatter.string(from: medicine.startDate).capitalized) au \(dateFormatter.string(from: medicine.endDate).capitalized)"
+        case .amount:
+            let capsuleMedicine = medicine as! CapsuleMedicineModel
+            return "\(capsuleMedicine.capsuleNumber) pillule(s)/jours"
+        case .capsuleWeight:
+            let capsuleMedicine = medicine as! CapsuleMedicineModel
+            return "\(capsuleMedicine.weight) mg"
+        case .volume:
+            return medicine.posologyString
+        case .patchDuration:
+            let patchMedicine = medicine as! PatchMedicineModel
+            return "\(patchMedicine.duration) h"
+        }
+    }
+
+    var body: some View {
+        HStack {
+            Image(systemName: logoName)
+                .font(.largeTitle)
+            VStack {
+                Text(cardType.rawValue)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Text(text)
+                    .font(.callout)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .frame(height: 80)
+        }
+        .padding(5)
+        .background(Color.white)
+        .foregroundStyle(color)
         .cornerRadius(12)
     }
 }
 
 #Preview {
     let vm = MedecineViewModel()
-    MedicineDetailView(medicineId: vm.medicines[1].id)
+    MedicineDetailView(path: .constant([]),medicineId: vm.medicines[0].id)
         .environment(vm)
 }

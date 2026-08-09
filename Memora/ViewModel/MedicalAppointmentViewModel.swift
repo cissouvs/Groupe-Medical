22e83b16000bd487487b0afcd0968fa89f948e8d
Gillian <pasquettegillian@sfr.fr>
2026-08-09 22:53:17 +0200

    add mock datas for medical appointments

---
+++ Memora/ViewModel/MedicalAppointmentViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  MedicalAppointmentViewModel.swift
+//  Memora
+//
+//  Created by Apprenant76 on 09/08/2026.
+//
+
+import Foundation
+
+let mockAppointments: [MedicalAppointmentModel] = [
+        MedicalAppointmentModel(
+            name: "Dr Martin",
+            profilePicture: "drMartin",
+            specialty: .generalist,
+            date: Date(),
+            adress: "8 Allée des Platanes, 46800 Montcuq-en-Quercy-Blanc",
+            phoneNumber: "0606060606",
+            emailAdress: "ledocteur@medecin.fr"
+        )
+    ]

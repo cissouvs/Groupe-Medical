3a2655be957f0211ddaa9c0289b5ce8d41ffcf5c
Gillian <pasquettegillian@sfr.fr>
2026-08-09 22:23:04 +0200

    Add MedicalAppointmentModel

---
+++ Memora/Models/MedicalAppointmentModel.swift
@@ -0,0 +1,33 @@
+//
+//  MedicalAppointmentModel.swift
+//  Memora
+//
+//  Created by Apprenant76 on 09/08/2026.
+//
+
+import Foundation
+
+enum MedicalSpeciality: String, Identifiable, CaseIterable {
+    case generalist = "Médecin généraliste"
+    case neurologist = "Neurologiste"
+    case geriatrician = "Gériatre"
+    case psychiatrist = "Psychiatre"
+    case neuropsychologist = "Neuropsychologue"
+    case occupationalTherapist = "Ergothérapeute"
+    case psychomotrician = "Psychomtricien"
+    case nurse = "Infirmier"
+    case other = "Autre"
+
+    var id: Self { self }
+}
+
+struct MedicalAppointmentModel: Identifiable {
+    var id: UUID = UUID()
+    var name: String
+    var profilePicture: String
+    var specialty: MedicalSpeciality
+    var date: Date
+    var adress: String
+    var phoneNumber: String?
+    var emailAdress: String?
+}

0becf043d1e61b4e4fc3fe18985da5b1ea25c035
Leskeu <pro.theotherage@gmail.com>
2026-08-11 11:27:05 +0200

    change the menu to  a popover

--- Memora/UI/PlacesListView.swift
+++ Memora/UI/PlacesListView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct PlacesListView: View {
     @State private var vm = PlacesListViewModel()
+    @State private var showPopover = false
     
     var body: some View {
         NavigationStack {
@@ -25,25 +26,40 @@ struct PlacesListView: View {
                 .navigationTitle("Lieux fréquentés")
             }
             .toolbar {
-                ToolbarItem {
-                    Menu {
-                        ForEach(DifferentPlaces.allCases) { type in
-                            Button {
-                                vm.selectedFilter = type
-                            } label: {
-                                if vm.selectedFilter == type {
-                                    Text(type.rawValue)
-                                        .foregroundStyle(.tagRed)
-                                        .font(.title)
-                                } else {
-                                    Text(type.rawValue)
-                                        .foregroundStyle(.mainText)
-                                        .font(.title)
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        showPopover.toggle()
+                    }label: {
+                        Image(systemName: "line.3.horizontal.decrease.circle")
+                            .foregroundStyle(.mainText)
+                    }.popover(isPresented: $showPopover) {
+                        VStack {
+                            Text("Liste des filtres")
+                                .bold()
+                                .font(.title)
+                            ForEach(DifferentPlaces.allCases) { type in
+                                Button {
+                                    vm.selectedFilter = type
+                                } label: {
+                                        Text(type.rawValue)
+                                            .foregroundStyle(vm.selectedFilter == type ? .accent: .mainText)
+                                            .font(.title3)
+                                            .bold()
+                                            .padding(.horizontal, 40)
+                                            .padding(.vertical, 10)
+                                            .frame(width: 300, height: 70)
+                                            .background(.white)
+                                            .cornerRadius(40)
+                                            .font(.title2)
+
                                 }
                             }
-                        }
-                    } label: {
-                        Label("Filtre", systemImage: "line.3.horizontal.decrease.circle")
+                        } .frame(width: 300, height: 350)
+                            .padding()
+                            .background(Color.background)
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
+                            .presentationCompactAdaptation(.popover)
                     }
                 }
             }

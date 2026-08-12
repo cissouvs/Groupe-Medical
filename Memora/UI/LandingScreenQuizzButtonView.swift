12ca22b49aa9da0274cd649f628564c968ab9306
cissouvs <illiencyriac13@gmail.com>
2026-08-12 15:14:05 +0200

    upp design btn landingScreen

--- Memora/UI/LandingScreenQuizzButtonView.swift
+++ Memora/UI/LandingScreenQuizzButtonView.swift
@@ -11,19 +11,31 @@ struct LandingScreenQuizzButtonView: View {
 
     var body: some View {
         HStack(alignment: .center) {
-            Image(systemName: "questionmark.square")
+            Image(systemName: "questionmark.square.fill")
+                .font(.system(size: 50))
                 .clipShape(.rect)
                 .frame(width: 70, height: 75)
                 .background(.supportBlue)
                 .cornerRadius(20)
                 .font(.title)
-            Text("Accéder à vos quizz")
-                .foregroundStyle(Color.background)
-                .font(.custom("Lexend-Regular", size: 20))
+            VStack(alignment: .leading) {
+                Text("Accéder à vos quizz")
+                    .font(.custom("Lexend-Regular", size: 20))
+                Text("Testez vos connaisances et progressez chaque jour")
+                    .font(.custom("Lexend-Regular", size: 12))
+            }
+            .foregroundStyle(.mainText)
+            .padding(.horizontal, 20)
+            Image(systemName: "chevron.right")
+                .padding(8)
+                .frame(width: 30, height: 30)
+                .background(.supportBlue)
+                .cornerRadius(100)
         }
+        .foregroundStyle(.whiteBackground)
         .padding(10)
         .frame(maxWidth: .infinity)
-        .background(.secondText)
+        .background(.tagBlue.opacity(0.5))
         .cornerRadius(20)
         .tint(.mainText)
     }
